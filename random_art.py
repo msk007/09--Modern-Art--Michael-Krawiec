25dbf4560d12ce82d9cc9ad761e51d34633805c2
msk007 <krawiec515@gmail.com>
2015-09-14 22:36:06 -0400

    changed file
    changed file to make random art. added expressions

--- random_art.py
+++ random_art.py
@@ -1,4 +1,5 @@
-import random
+from random import random
+import math
 
 # Your job is to create better version of create_expression and
 # run_expression to create random art.
@@ -8,10 +9,13 @@ import random
 def create_expression():
     """This function takes no arguments and returns an expression that
     generates a number between -1.0 and 1.0, given x and y coordinates."""
-    expr1 = lambda x, y: (x + y)/2
-    expr2 = lambda x, y: (x - y)/2
-    expr3 = lambda x, y: x * y
-    return random.choice([expr1, expr2, expr3])
+    expr1 = lambda x, y: (x + y)/2 * cos(y) * tan(5 ** 4) *cos(pi * sin(pi * cos))
+    expr2 = lambda x, y: (x - y)/2 * random()
+    expr3 = lambda x, y: x * y/4 * random()
+    expr4 = lambda x, y: tan(6 * x) * cos(x)
+    expr5 = lambda y, x: (sin(pi)) + tan(y**2)
+    return random.choice([expr1, expr2, expr3, expr4, expr5])
+
 
 
 def run_expression(expr, x, y):
